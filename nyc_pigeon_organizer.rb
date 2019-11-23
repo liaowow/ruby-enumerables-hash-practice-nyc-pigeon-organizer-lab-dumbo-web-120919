0e68842fffd5091dd3a420a0c6952f3a5ee7f86e
Annie Liao <liaowow@yahoo.com>
2019-11-23 21:25:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,33 @@
 def nyc_pigeon_organizer(data)
   new_list = {}
-  pigeon_hash = {}
-  inner_arr = []
-  data.each do |key, value|
-    value.each do |inner_key, inner_value|
-      inner_value.each do |pigeon_name| 
-          pigeon_hash[key] = inner_arr
-          new_list[pigeon_name] = pigeon_hash
+
+  unique_pigeon_arr = []
+  data.each do |attribute, hash_of_features|
+    hash_of_features.each do |feature, name_arr|
+      name_arr.each do |pigeon_name|
+        if !unique_pigeon_arr.include?(pigeon_name)
+          unique_pigeon_arr << pigeon_name
+        end
       end
     end
   end
+  
+  unique_pigeon_arr.each do |unique_pigeon_name|
+    new_list[unique_pigeon_name] = Hash.new {|k. v| k[v] = []}
+    
+    data.each do |attribute, hash_of_features|
+      
+      arr_of_features = new_list[unique_pigeon_name][attribute]
+      
+      hash_of_features.each do |feature, name_arr|
+        name_arr.each do |pigeon_name|
+          if pigeon_name == unique_pigeon_name
+            arr_of_features << feature
+          end
+        end
+      end
+    end
+  end
+
   new_list
 end
