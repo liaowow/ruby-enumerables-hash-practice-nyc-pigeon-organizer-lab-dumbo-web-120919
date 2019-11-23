152cfb227b43e7a407290d62384aae05e56655ee
Annie Liao <liaowow@yahoo.com>
2019-11-23 19:35:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,12 @@ def nyc_pigeon_organizer(data)
   new_list = {}
   data.each do |key, value|
     value.each do |inner_key, inner_value|
-      new_list[inner_value] = {
-        :key => inner_key
-      }
+      inner_value.each do |pigeon_name| 
+        new_list[pigeon_name] = {
+
+        }
+      end
+
     end
   end
   new_list
